25268e5fbd09f1be4b1697d79910e9a3332e4c99
Helen Yates <shadow.ecco@gmail.com>
2021-01-12 23:34:30 +0000

    update css on love generator

--- javascript-projects/Love-Generator/lovegenerator.js
+++
@@ -1,5 +0,0 @@
-var person1 = prompt("What is your name?");
-var person2 = prompt("What is your crush's name?");
-var loveScore = Math.random() * 100;
-loveScore = Math.floor(loveScore) + 1;
-alert("Your love score is " + loveScore + " %!")
